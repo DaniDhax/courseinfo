1cbdaab5f8fba9ef23256647f34c646cba79734b
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-03-28 14:44:34 +0000

    Ejercicio 1.1 resuelto.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+     <p>{props.part} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.numero}</p>
+  )
+}
+
 const App = () => {
+  // const-definitions
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
@@ -12,19 +35,47 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1} /> 
+      <Content part={part2} exercises={exercises2} /> 
+      <Content part={part3} exercises={exercises3} /> 
+      <Total numero={exercises1 + exercises2 + exercises3} />
+
     </div>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+
+// const App = () => {
+//   const course = 'Half Stack application development'
+//   const part1 = 'Fundamentals of React'
+//   const exercises1 = 10
+//   const part2 = 'Using props to pass data'
+//   const exercises2 = 7
+//   const part3 = 'State of a component'
+//   const exercises3 = 14
+
+//   return (
+//     <div>
+//       <h1>{course}</h1>
+//       <p>
+//         {part1} {exercises1}
+//       </p>
+//       <p>
+//         {part2} {exercises2}
+//       </p>
+//       <p>
+//         {part3} {exercises3}
+//       </p>
+//       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//     </div>
+//   )
+// }
+
+// ReactDOM.render(<App />, document.getElementById('root'))
