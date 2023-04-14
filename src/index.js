cac500781c21d5bac05b464dfa18ef531139147b
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-04-14 11:32:15 +0000

    Part1: Re-renderizado de la página: Contador

--- src/index.js
+++ src/index.js
@@ -1,43 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 
-  const App = ({counter}) => {
-    const estilo = {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      color: 'red',
-      backgroundColor: 'yellow',
-      fontsize: '20px',
-      borderStyle: 'solid',
-      borderWidth: '1px',
-      borderColor: 'black'
-    }
 
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
+const App = (props) => {
+  
+  const [ counter, setCounter ] = useState(0)
+
+  // setTimeout(
+  //   () => setCounter(counter+1),
+  //   1000
+  // )
 
   return (
-    <div style={estilo}>
-      <h1>Contador</h1>
-      <p style={{ fontWeight: 900, fontSize: '24px' }}>{`${hours}:${minutes}:${seconds}`}</p>
+    <div>
+      <p>{counter}</p>
     </div>
   )
 }
 
-let counter = 1
 
-setInterval(
-  () => {
-    ReactDOM.render (<App counter={counter}/>, document.getElementById('root'))
-    counter += 1
-  },
-  1000
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 
-//ReactDOM.render(<App counter={counter}/>, document.getElementById('root'))
-//ReactDOM.render(<App />, document.getElementById('root'))
