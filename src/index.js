c83d214c731d644b2bd01bfad9045aacb927dd61
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-04-17 15:29:47 +0000

    Part1.d: Estado complejo.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,18 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom"
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
-
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter (0)
-
-  return (
-    <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text='plus'/>
-      <Button handleClick={setToZero} text='zero'/>
-      <Button handleClick={decreaseByOne} text='minus'/>
-    </div>
-  )
+    return (
+        <div>
+            {left}
+            <button onClick={() => setLeft(left + 1)}>left</button>
+            <button onClick={() => setRight(right + 1)}>right</button>
+            {right}
+        </div>
+    )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-
+ReactDOM.render (<App />, document.getElementById('root'))
