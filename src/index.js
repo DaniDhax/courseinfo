98be4c668ba8488980d6c6e3ab3e518dafac79a9
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-04-17 14:02:36 +0000

    Part1: Manejo de eventos.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom"
 
 
 const App = (props) => {
-  
-  const [ counter, setCounter ] = useState(0)
 
-  // setTimeout(
-  //   () => setCounter(counter+1),
-  //   1000
-  // )
+  const [counter, setCounter] = useState(0)
+
+  const handleClick = () => {
+    console.log('Clicked')
+  }
 
   return (
     <div>
       <p>{counter}</p>
+      <button onClick={() => setCounter(counter + 1)}>plus</button>
+      <button onClick={() => setCounter(0)}>zero</button>
     </div>
   )
 }
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
