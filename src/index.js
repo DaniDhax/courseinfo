6df817e6781f0fa2f0b871ecb7127ea38fc48b22
DaniDhax <111738016+DaniDhax@users.noreply.github.com>
2023-04-18 11:45:28 +0000

    Part1.d: Manejo de matrices.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React, { useState } from "react"
+import React, { useState} from "react"
 import ReactDOM from "react-dom"
 
 const App = () => {
     const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
+    const [right, setRight] = useState (0)
+    const [allClicks, setAll] = useState([])
+
+    const handleLeftClick = () => {
+        setLeft (left + 1)
+        setAll (allClicks.concat('L'))
+    }
+
+    const handleRightClick = () => {
+        setRight (right + 1)
+        setAll (allClicks.concat('R'))
+    }
 
     return (
         <div>
             {left}
-            <button onClick={() => setLeft(left + 1)}>left</button>
-            <button onClick={() => setRight(right + 1)}>right</button>
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
             {right}
+            <br></br>
+            <p>{allClicks.join('-')}</p>
+
         </div>
     )
 }
 
-ReactDOM.render (<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById ('root'))
